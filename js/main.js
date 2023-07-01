5faf79edec5d3e5cba2ff75d517aac743c47b8ed
JuanMR <juanmr.093@gmail.com>
2023-07-01 09:59:05 -0300

    Entrega Final

--- js/main.js
+++ js/main.js
@@ -1,157 +1,4 @@
-//constructores para manejar los ingresos que haga el usuario
-
-class Tarta {
-	constructor(sabor, precio, tamaño, stock) {
-		this.sabor = sabor;
-		this.precio = precio;
-		this.tamaño = tamaño;
-		this.stock = stock; //? o stockTarta?
-	}
-	venderTarta = (cant) => {
-		//ver como darle funcionalidad para los stock de todos los productos
-		if (cant <= stock) {
-			this.stock = this.stock - cant;
-		} else alert("La cantidad es mayor a la disponible");
-	};
-}
-class Facturas {
-	constructor(tipo, cantidad, stock) {
-		this.tipo = tipo;
-		this.cantidad = cantidad;
-		this.stock = stock; //o stockFacturas?
-	}
-	venderFacturas = (cant) => {
-		//ver como darle funcionalidad para los stock de todos los productos
-		this.stock = this.stock - cant;
-	};
-}
-const encargoTorta = [];
-class Torta {
-	constructor(bizcochuelo, relleno, cobertura, tamaño) {
-		this.bizcochuelo = bizcochuelo;
-		this.relleno = relleno;
-		this.cobertura = cobertura;
-		this.tamaño = tamaño;
-	}
-}
 const cobertura = ["Salta", "San Lorenzo", "Tres Cerritos", "San Luis"];
-//empiezo a construir mi base de arrays de tartas, de a poco iré sumando otros productos.
-/* const productos = [
-	{
-		id: 1,
-		tipo: "tarta",
-		sabor: "Cabsha",
-		precio: "1200",
-		cantidad: 0,
-		tamañoGrande: "28cm diametro",
-		tamañoChico: "10cm diametro",
-		imagen: "./img/cabsha.jpg",
-	},
-	{
-		id: 2,
-		tipo: "tarta",
-		sabor: "Frutillas",
-		precio: "1500",
-		cantidad: 0,
-		tamañoGrande: "28cm diametro",
-		tamañoChico: "10cm diametro",
-		imagen: "./img/frutilla1.jpg",
-	},
-	{
-		id: 3,
-		tipo: "tarta",
-		sabor: "Pastafrola",
-		precio: "1100",
-		cantidad: 0,
-		tamañoGrande: "28cm diametro",
-		tamañoChico: "10cm diametro",
-		imagen: "./img/pastafrola1.jpg",
-	},
-	{
-		id: 4,
-		tipo: "tarta",
-		sabor: "AppleCrumble",
-		precio: "1500",
-		cantidad: 0,
-		tamañoGrande: "28cm diametro",
-		tamañoChico: "10cm diametro",
-		imagen: "./img/manzana1.jpg",
-	},
-	{
-		id: 5,
-		tipo: "tarta",
-		sabor: "LemonPie",
-		precio: "1300",
-		cantidad: 0,
-		tamañoGrande: "28cm diametro",
-		tamañoChico: "10cm diametro",
-		imagen: "./img/lemonpie1.jpg",
-	},
-	{
-		id: 6,
-		tipo: "tarta",
-		sabor: "Brownie",
-		precio: "1500",
-		cantidad: 0,
-		tamañoGrande: "28cm diametro",
-		tamañoChico: "10cm diametro",
-		imagen: "./img/brownie1.jpg",
-	},
-	{
-		id: 7,
-		tipo: "tarta",
-		sabor: "Ricota con dulce de leche",
-		precio: "1750",
-		cantidad: 0,
-		tamañoGrande: "28cm diametro",
-		tamañoChico: "10cm diametro",
-		imagen: "./img/ricota1.jpg",
-	},
-	{
-		id: 8,
-		tipo: "tarta",
-		sabor: "Cheesecake",
-		precio: "1900",
-		cantidad: 0,
-		tamañoGrande: "28cm diametro",
-		tamañoChico: "10cm diametro",
-		imagen: "./img/cheesecake.jpg",
-	},
-	{
-		id: 9,
-		tipo: "facturas",
-		sabor: "Medialunas",
-		precio: "150",
-		cantidad: 0,
-		imagen: "./img/medialuna1.jpg",
-	},
-	{
-		id: 10,
-		tipo: "facturas",
-		sabor: "Cañioncitos",
-		precio: "180",
-		cantidad: 0,
-		imagen: "./img/canioncito.jpg",
-	},
-	{
-		id: 11,
-		tipo: "facturas",
-		sabor: "Conitos",
-		precio: "180",
-		cantidad: 0,
-		imagen: "./img/conitos.jpg",
-	},
-	{
-		id: 12,
-		tipo: "facturas",
-		sabor: "Chipa",
-		precio: "220",
-		cantidad: 0,
-		imagen: "./img/chipa.jpg",
-	},
-];
- */
-
 let carritoCompra = JSON.parse(localStorage.getItem("carrito")) || [];
 let viendoCarrito = document.getElementById("cart");
 let agregarTotal = document.getElementById("total");
@@ -159,70 +6,72 @@ let finalizarCompra = document.querySelector(".finalizar");
 const inputSearch = document.querySelector(".form-control");
 const contenedor = document.getElementById("container");
 const zonas = document.getElementById("zonas");
-const btnSearch = document.querySelector(".btn");
-
-//array para trabajar desde el LS
-/* const productosLS = JSON.parse(localStorage.getItem("productos")); */
-/* const productosLS = []; */
+const btnSearch = document.querySelector(".btn-busqueda");
 
 //funcion guardar productos en el LS
 function guardarEnLS(clave, arr) {
 	localStorage.setItem(clave, JSON.stringify(arr));
 }
 
-/* let carritoLS = JSON.parse(localStorage.getItem(productosLS)) || productosLS; */
-
+//funcion de busqueda
+function filtrar(arr, filtro, param) {
+	return arr.filter((el) => {
+		if (param == "precio") {
+			return el.precio && el.precio <= parseFloat(filtro);
+		} else if (el[param]) {
+			return el[param].toLowerCase().includes(filtro.toLowerCase());
+		}
+		return false;
+	});
+}
 //evento en el input
 
 inputSearch.addEventListener("keyup", () => {
 	inputSearch.value;
 });
 
-//asigno evento de busqueda
-btnSearch.addEventListener("click", (e) => {
-	e.preventDefault();
-	const busq = busqueda(productos, inputSearch.value);
-	console.log(busq);
-});
-
-/* localStorage.getItem("productos")
-	? (dulzuras = JSON.parse(localStorage.getItem("productos")))
-	: (carritoCompra = productos); */
-
-//funcion de busqueda
-function filtrar(arr, filtro, param) {
-	return arr.filter((el) => {
-		if (param == "precio") {
-			return el.precio <= parseFloat(filtro);
-		} else {
-			return el[`${param}`].includes(filtro);
-		}
+//fetch para busqueda
+let productos = [];
+// fetch para trabajar array desde .json
+fetch("./data/db.json")
+	.then((response) => response.json())
+	.then((data) => {
+		productos = data;
+		dibujarProductos(productos);
+		renderizarCarrito();
+	})
+	.catch((error) => {
+		console.log("Error al obtener datos", error);
 	});
-}
+
+// Función de búsqueda
+const busqueda = function (filtro) {
+	if (filtro.trim() === "") {
+		// si el campo de búsqueda está vacío, muestra todos los productos
+		dibujarProductos(productos);
+	} else {
+		const productosFiltrados = filtrar(productos, filtro, "sabor");
+		dibujarProductos(productosFiltrados);
+	}
+};
 
 // funcion para dibujar el html
 const dibujarProductos = (productos) => {
 	contenedor.innerHTML = "";
 	productos.forEach((producto) => {
 		let card = document.createElement("div");
-		card.classList.add("card", "col.sm-12", "col-lg-3");
-		let contenido = `  <img src="${producto.imagen}" class="card-img-top" alt="...">
+		card.classList.add("card", "col-sm-6", "col-lg-3");
+		let contenido = `  <img src="${producto.imagen}" class="card-img-top animate__pulse" alt="...">
 		<div class="card-body">
 		  <h5 class="card-title">${producto.sabor}</h5>
 		  <p class="card-text">$${producto.precio}</p>
 		  <button id="agregar-${producto.id}" class="btn btn-bs-warning-bg-subtle agregar-btn" >Agregar Dulzura</button>
 		</div>`;
-		// Agrego la card al contenedor
 		card.innerHTML = contenido;
 		contenedor.appendChild(card);
-		// Ahora la card está en el DOM. El botón ya existe, por lo tanto lo capturo
-		const botones = document.getElementById("agregar-${producto.id}");
-		// Agrego evento al botón capturado.
-		botones.forEach((boton) => {
-			// Si hacemos clic en el botón, se agrega al carrito
-			boton.addEventListener("click", () => {
-				aniadirCarrito(productos, producto.id);
-			});
+		const boton = document.getElementById(`agregar-${producto.id}`);
+		boton.addEventListener("click", () => {
+			aniadirCarrito(productos, producto.id);
 		});
 	});
 };
@@ -232,46 +81,17 @@ let totalCarrito = carritoCompra.reduce(
 	0
 );
 
-//funcion  de filtro
-const busqueda = function (arr, filtro) {
-	const encontrado = arr.filter((el) => {
-		return el.sabor.includes(filtro);
-	});
-	dibujarProductos(encontrado);
-};
-// funcion para añadir productos al carrito
-/* let aniadirCarrito = (id) => {
-	const producto = productos.find((el) => el.id == id); //arreglar para q me acepte mas de uno y se vayan sumando
-	const productoExistente = carritoCompra.find((el) => el.id === producto.id);
-
-	if (productoExistente) {
-		// Si el producto ya existe, incrementar su cantidad
-		productoExistente.cantidad++;
-	} else {
-		// Si es un nuevo producto, añadirlo al carrito con cantidad 1
-		producto.cantidad = 1;
-		carritoCompra.push(producto);
-	}
-
-	guardarEnLS("carrito", carritoCompra);
-	renderizarCarrito();
-}; */
+// funcion para añadir productos al carrito y se guarda en LS
 
 const aniadirCarrito = (productos, id) => {
-	// Si el producto no está en el carrito, lo agregamos
 	if (!carritoCompra.some((producto) => producto.id === id)) {
-		// Buscamos el producto en el array de productos
 		const producto = productos.find((producto) => producto.id === id);
-		// Agregamos un nuevo objeto con el contenido del producto y un campo cantidad en 1.
 		carritoCompra.push({ ...producto, cantidad: 1 });
 	} else {
-		// Si el producto está en el carrito, lo buscamos y le incrementamos las unidades
 		const producto = carritoCompra.find((producto) => producto.id === id);
 		producto.cantidad++;
 	}
-	// Guardamos el carrito en el localStorage para tenerlo actualizado si recargamos la página porque hicimos cambios
 	guardarEnLS("carrito", carritoCompra);
-	// Actualizamos la vista del carrito porque hemos hecho cambios
 	renderizarCarrito();
 };
 
@@ -334,7 +154,7 @@ const removerProducto = (index) => {
 	renderizarCarrito();
 };
 
-//guardar carrito en LS y finalizar compra
+//finalizar compra, se agrega formulario y se borra el carrito del LS
 
 async function finalizando() {
 	const { value: formValues } = await Swal.fire({
@@ -378,17 +198,27 @@ async function finalizando() {
 		focusConfirm: false,
 		showCancelButton: false,
 	});
-
 	if (formValues) {
-		setTimeout(() => {
-			Swal.fire("Gracias por tu compra").then(() => {
-				carritoCompra.splice(0); //borro datos del array carrito
-				localStorage.removeItem("carrito"); //borro el carrito
-				location.reload(); // Recargar la página
-			});
-		}, 1000);
+		//barra de espera ficticia
+		let barra = document.createElement("div");
+		barra.classList.add("progress");
+		barra.innerHTML = `<div class="indeterminate"></div>`;
+		document.body.appendChild(barra);
 	}
+	setTimeout(() => {
+		Swal.fire("Gracias por tu compra").then(() => {
+			carritoCompra.splice(0); // Borrar datos del array carrito
+			localStorage.removeItem("carrito"); // Borrar el carrito
+			location.reload(); // Recargar la página
+		});
+	}, 2000);
 }
+// Asigna el evento click al botón de búsqueda
+btnSearch.addEventListener("click", (e) => {
+	e.preventDefault();
+	const valorB = inputSearch.value;
+	busqueda(valorB);
+});
 
 // desestructura cobertura
 
@@ -400,29 +230,3 @@ let encontranos = () => {
 	zonas.innerHTML = `<div>Encontranos en ${zonaCobertura}</div>`;
 };
 encontranos();
-
-renderizarCarrito();
-fetch("./data/db.json")
-	.then((response) => response.json())
-	.then((productos) => {
-		dibujarProductos(productos);
-		renderizarCarrito();
-	});
-console.log(productos);
-/* fetch("./data/db.json")
-	.then((response) => response.json())
-	.then((data) => {
-		// Obtener los productos del objeto data
-		const productos = data.productos;
-
-		// Guardar los productos en el Local Storage
-		guardarEnLS("productos2", productos);
-
-		// Llamar a la función para dibujar los productos en el navegador
-
-		dibujarProductos(productos);
-	})
-	.catch((error) => {
-		console.log("Error al obtener los datos:", error);
-	});
- */
